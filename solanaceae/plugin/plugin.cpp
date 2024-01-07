2bf642666381e7b3760c601eccbb10a5240a952c
Green Sky <green@g-s.xyz>
2024-01-07 18:00:28 +0100

    no more windows.h when?

--- solanaceae/plugin/plugin.cpp
+++ solanaceae/plugin/plugin.cpp
@@ -7,7 +7,9 @@
 
 #if (defined(_WIN32) || defined(_WIN64))
 	#define WIN32_LEAN_AND_MEAN
+	#define NOMINMAX
 	#include <windows.h>
+	#undef NOMINMAX
 	#undef WIN32_LEAN_AND_MEAN
 #else
 	#include <dlfcn.h>
