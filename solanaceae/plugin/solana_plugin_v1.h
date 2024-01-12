b50269062172b013d21fcc38b3693d09f91a2154
Green Sky <green@g-s.xyz>
2024-01-12 17:45:44 +0100

    message components syncedby chagned and remote added

--- solanaceae/plugin/solana_plugin_v1.h
+++ solanaceae/plugin/solana_plugin_v1.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-#define SOLANA_PLUGIN_VERSION 7
+#define SOLANA_PLUGIN_VERSION 8
 
 #if defined(_MSC_VER) || defined(__MINGW32__)
 	#define SOLANA_PLUGIN_EXPORT __declspec(dllexport)
